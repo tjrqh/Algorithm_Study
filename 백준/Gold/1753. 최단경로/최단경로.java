065b7912c7986827917c51c4a0a99a23bddb42b8
boseok <95565584+tjrqh@users.noreply.github.com>
2024-10-28 14:06:36 +0900

    [Gold IV] Title: 최단경로, Time: 692 ms, Memory: 107060 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,96 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+    static ArrayList[] l;
+    static int[] dist;
 
-  static int v, e, k;
-  static ArrayList<Node>[] list;
-  static int[] dist;
-  static boolean[] visited;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine()) - 1;
 
-    v=Integer.parseInt(st.nextToken());
-    e=Integer.parseInt(st.nextToken());
+        l = new ArrayList[V];
+        dist = new int[V];
 
-    dist = new int[v + 1];
-    list = new ArrayList[v + 1];
-    visited = new boolean[v + 1];
-
-    for (int i = 1; i <= v; i++) {
-      list[i] = new ArrayList<>();
-    }
-    Arrays.fill(dist, Integer.MAX_VALUE); // Arrays.fill 은 배열의 모든 값을초기화 하는 것으로, dist 배열을 Integer.MAX_VALUE 값으로 초기화 시킨다.
-
-    k = Integer.parseInt(br.readLine());    // 시작 번호
-    dist[k] = 0;
-
-    for (int i = 0; i < e; i++) {
-      String[] tt = br.readLine().split(" ");
-
-      int a = Integer.parseInt(tt[0]);   // 노드 1
-      int b = Integer.parseInt(tt[1]);   // 노드 2
-      int w = Integer.parseInt(tt[2]);   // 거리
-
-      list[a].add(new Node(b, w));  // Node 에 값을 넣어준다 [Node{b=1, w=1}], [Node{b=2, w=2}], [Node{b=2, w=2}, Node{b=3, w=3}],[Node{b=3, w=4}],[Node{b=3, w=4}, Node{b=4, w=5}]
-
-    }
-    solve();
-    for (int i = 1; i <= v; i++) {
-      if (dist[i] == Integer.MAX_VALUE) {
-        System.out.println("INF");
-      } else {
-        System.out.println(dist[i]);
-      }
-    }
-  }
-
-  public static void solve() {
-    PriorityQueue<Node> pq = new PriorityQueue<>();
-
-    pq.add(new Node(k, 0));
-
-    while (!pq.isEmpty()) {
-      Node a = pq.poll();
-
-      if (visited[a.idx]) {
-        continue;
-      }
-      visited[a.idx] = true;
-      for (Node o : list[a.idx]) {
-        if (dist[o.idx] > dist[a.idx] + o.w) {
-          dist[o.idx] = dist[a.idx] + o.w;
-          pq.add(new Node(o.idx, dist[o.idx]));
+        for(int i = 0; i<V; i++) {
+            l[i] = new ArrayList<Node>();
+            dist[i] = Integer.MAX_VALUE;
         }
-      }
+
+        for(int i = 0; i<E; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+
+            l[u].add(new Node(v, w));
+        }
+
+        dijkstra(K);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<V; i++){
+            if(dist[i] == Integer.MAX_VALUE)
+                sb.append("INF").append("\n");
+            else
+                sb.append(dist[i]).append("\n");
+        }
+
+        System.out.print(sb);
     }
-  }
+
+    static void dijkstra(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        dist[start] = 0;
+        pq.add(new Node(start, 0));
+
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            int len = l[now.v].size();
+            for(int i = 0; i<len; i++){
+                Node next = (Node)l[now.v].get(i);
+
+                if(dist[next.v]>now.w + next.w){
+                    dist[next.v] = now.w + next.w;
+                    pq.add(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+    }
 }
 
-class Node implements Comparable<Node> {
+class Node implements Comparable<Node>{
+    int v, w;
 
-  int idx, w;
+    public Node(int v, int w){
+        this.v = v;
+        this.w = w;
+    }
 
-  public Node(int idx, int w) {     // 노드에 저장
-    this.idx = idx;
-    this.w = w;
-  }
-
-  public int compareTo(Node o) {
-    return this.w - o.w;
-  }
+    @Override
+    public int compareTo(Node n){
+        return this.w - n.w;
+    }
 }
-
-
-/*
-ArrayList 와 LinkedList 의 시간 차이
-
-
- */
